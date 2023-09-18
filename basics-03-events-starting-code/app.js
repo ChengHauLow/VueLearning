0f617cebeaa3182c1cbf4de4e891e7d746e6c397
unknown <lowlovetwice@gmail.com>
2023-09-18 16:37:35 +0800

    Added button clicked count function

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,18 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      sumBtnClicked: 0,
     };
   },
   methods: {
     addCount(e) {
       let value = +e.target.dataset.step || 1;
+      this.sumBtnClicked++;
       return (this.counter += value);
     },
     reduceCount() {
+      this.sumBtnClicked++;
       this.counter > 0 ? (this.counter -= 1) : (this.counter = 0);
     },
     zeroCount() {
       this.counter = 0;
+      this.sumBtnClicked = 0;
     },
   },
 });
